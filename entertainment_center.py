9afd58641168c49b6d52a398ed865b092284e4a9
Yosua1011 <Yosua1011@gmail.com>
2016-10-24 11:42:08 +0700

    Add movie valid ratings

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,10 +1,15 @@
+import fresh_tomatoes
 import media
 
 toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", "http://img.lum.dolimg.com/v1/images/open-uri20150422-20810-m8zzyx_5670999f.jpeg?region=0,0,300,450", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-avatar = media.Movie("Avatar", "A marine on an alien planet", "http://www.fandomfollowing.com/wp-content/uploads/2016/02/avatar-wallp-1920x1080.jpeg", "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
+avatar = media.Movie("Avatar", "A marine on an alien planet", "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg", "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
 
 the_founder = media.Movie("The Founder", "A man who build McDonald Empire", "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Founder_poster.jpg", "https://www.youtube.com/watch?v=AX2uz2XYkbo")
 
 ratatouille = media.Movie("Ratatouille", "A mouse who dreamed to be a chef", "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg", "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+movies = [toy_story, avatar, the_founder, ratatouille]
+#fresh_tomatoes.open_movies_page(movies)
+print (media.Movie.VALID_RATINGS)
 

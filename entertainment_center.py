6854bfa97bd5b09545572601b9a02e2a6167f912
Yosua1011 <Yosua1011@gmail.com>
2016-10-24 07:49:10 +0700

    Create media.py & entertainment_center.py

---
+++ entertainment_center.py
@@ -0,0 +1,5 @@
+import media
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", "http://img.lum.dolimg.com/v1/images/open-uri20150422-20810-m8zzyx_5670999f.jpeg?region=0,0,300,450", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+print(toy_story.storyline)

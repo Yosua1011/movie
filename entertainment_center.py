bff72bfd075e876397cfe3e722150a097fe0cf71
Yosua1011 <Yosua1011@gmail.com>
2016-10-24 08:07:21 +0700

    Add Avatar instance

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,3 +3,5 @@ import media
 toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", "http://img.lum.dolimg.com/v1/images/open-uri20150422-20810-m8zzyx_5670999f.jpeg?region=0,0,300,450", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 print(toy_story.storyline)
+avatar = media.Movie("Avatar", "A marine on an alien planet", "http://www.fandomfollowing.com/wp-content/uploads/2016/02/avatar-wallp-1920x1080.jpeg", "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
+print(avatar.storyline)
